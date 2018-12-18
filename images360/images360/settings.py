1ca0df547f797c0a1dce6266b062f280b741c5fd
hamdi <javs_shao@163.com>
2018-12-18 16:31:08 +0800

    最大爬取页数

--- images360/images360/settings.py
+++ images360/images360/settings.py
@@ -88,3 +88,6 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# 最大爬取页数
+MAX_PAGE = 50
