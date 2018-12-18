e58e3121fdc50608efdff36c65434ac14a5414d8
hamdi <javs_shao@163.com>
2018-12-18 16:44:10 +0800

    编写_请求地址 & 页码

--- images360/images360/spiders/images.py
+++ images360/images360/spiders/images.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from urllib.parse import urlencode
+
 import scrapy
+from scrapy import Request
 
 
 class ImagesSpider(scrapy.Spider):
@@ -7,5 +10,15 @@ class ImagesSpider(scrapy.Spider):
     allowed_domains = ['images.so.com']
     start_urls = ['http://images.so.com/']
 
+    def start_requests(self):
+        data = {'ch': 'viedo', 'listtype': 'new','temp':'1'}
+        base_url = 'https://image.so.com/zj?'
+        for page in range(1, self.settings.get('MAX_PAGE') + 1):
+            data['sn'] = page * 30
+            params = urlencode(data)
+            url = base_url + params
+            yield Request(url, self.parse)
+
     def parse(self, response):
         pass
+
